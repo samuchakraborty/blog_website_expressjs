f0c16a0626804f148e6a8b498c026143ad0a2cc0
samuchakraborty <samuckb@gmail.com>
2021-02-17 23:16:53 +0600

    done dynamic route by ejs

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -12,7 +13,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
@@ -21,8 +22,8 @@ const postArray = [];
 
 
 
-app.get('/', function(req, res){
-  res.render('home', { homecontent: homeStartingContent, post: postArray});
+app.get('/', function (req, res) {
+  res.render('home', { homecontent: homeStartingContent, post: postArray });
 })
 
 
@@ -31,41 +32,55 @@ app.get('/about', function (req, res) {
 })
 
 app.get('/contact', function (req, res) {
-  res.render('contact', { contactContent: contactContent,  });
+  res.render('contact', { contactContent: contactContent, });
 })
 
 app.get('/compose', function (req, res) {
-  res.render('compose', );
+  res.render('compose',);
 });
 
 app.post('/compose', function (req, res) {
   let name = req.body.inputText;
   let postTextFeild = req.body.postText;
- 
 
-  let postObject= {
-title: name,
-letter: postTextFeild,
+  const postObject = {
+    title: name,
+    letter: postTextFeild,
 
   };
 
-
   postArray.push(postObject);
 
-  
-
-
-
   res.redirect('/');
- 
+
+})
+
+
+app.get('/post/:books', function (req, res){
+//console.log(req.params.books);
+//  console.log(postArray[0]['title']);
+
+  var check = _.lowerCase(req.params.books);
+  postArray.forEach(function(i){
+    if (check === _.lowerCase(i.title)) {
+      
+    console.log(check +' match found');
+      res.render('post', { post: {tit: i.title , des: i.letter}});
+  
+  
+  }
+  else{
+    console.log(check +"  not match");
+  }
 })
 
 
 
+});
 
 
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
